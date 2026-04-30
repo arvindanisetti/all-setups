d573cd67bf29ca8aefe8db2529641dae93f32010
arvindanisetti <102297339+arvindanisetti@users.noreply.github.com>
2026-04-30 09:57:57 +0530

    Comment out PATH export in docker-compose.sh
    Comment out the bashrc configuration commands.

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,6 +1,6 @@
-vim .bashrc
-export PATH=$PATH:/usr/local/bin/
-source .bashrc
+# vim .bashrc
+# export PATH=$PATH:/usr/local/bin/
+# source .bashrc
 
 # Download the current stable release of Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
